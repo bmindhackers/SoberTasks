2cfd86c439ff25dc0a14419d926a957309d60c15
Burhan Ahmad <bmindhackers@hotmail.com>
2024-12-05 22:48:32 -0500

    Added project files and untracked directories

---
+++ SoberTasks/SoberTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoberTasksApp.swift
+//  SoberTasks
+//
+//  Created by Ch Burhan on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SoberTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ SoberTasksTests/SoberTasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoberTasksTests.swift
+//  SoberTasksTests
+//
+//  Created by Ch Burhan on 12/5/24.
+//
+
+import Testing
+@testable import SoberTasks
+
+struct SoberTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
